8ffce265af8d77c24b77d94078d71b992950607e
くろっく <96430996+RC-Clock@users.noreply.github.com>
2022-03-06 18:50:23 +0900

    Update cdb.php

--- chat/config/database/cdb.php
+++ chat/config/database/cdb.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost:3306;dbname=chat;charset=utf8','root','digj-jhs038');
+$pdo = new PDO('mysql:host=SQLサーバーのアドレス;dbname=chat;charset=utf8','アカウント名','パスワード');
 ?>
